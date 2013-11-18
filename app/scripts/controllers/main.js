49c702917f5fd4915c1c144df92b2c938daec5e6
giuseppe <damicogiuseppe77@gmail.com>
2013-11-18 18:55:31 +0100

    0.1.1 filters over line's dress

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,23 @@
 'use strict';
-
+var debug = function(s) {
+    console.log(s);
+}
 angular.module('dollsdressesApp')
   .controller('MainCtrl', function ($scope, $http) {
     $http.get('/api/dress').success(function(dresses) {
       $scope.dresses = dresses;
+        var lines = [];
+        for (var i=0;i<dresses.length;i++) {
+            if(lines.indexOf(dresses[i].linea)<0) {
+                lines.push(dresses[i].linea)
+                
+            }
+            
+        };
+        $scope.lines = lines;
+        $scope.lineProp = '';
+        debug('linee');
+        debug(lines);
+        
     });
   });
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -12,17 +12,37 @@ describe('Controller: MainCtrl', function () {
   // Initialize the controller and a mock scope
   beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
     $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('/api/awesomeThings')
-      .respond(['HTML5 Boilerplate', 'AngularJS', 'Karma', 'Express']);    
+    $httpBackend.expectGET('/api/dress')
+      .respond([
+{
+_id: "5289e22b1ab5bf98019b0518",
+descrizione: "Abito lana bianco",
+linea: "fashion",
+main_foto: "dress1.jpg",
+nome: "Abito lana bianco",
+gallery: [
+"dress01.jpg",
+"dress02.jpg"
+]
+},
+{
+_id: "5289e9db3b9f158b7d041332",
+nome: "Abito classico 1",
+descrizione: "Altro abito",
+linea: "fashion",
+main_foto: "dress2.jpg",
+gallery: [ ]
+}
+]);    
     scope = $rootScope.$new();
     MainCtrl = $controller('MainCtrl', {
       $scope: scope
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
+  it('should attach a list of dress to the scope', function () {
     expect(scope.awesomeThings).toBeUndefined();
     $httpBackend.flush();
-    expect(scope.awesomeThings.length).toBe(4);
+    expect(scope.dresses.length).toBe(2);
   });
 });
